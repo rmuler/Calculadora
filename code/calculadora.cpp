af54778f02acd06b36ba32fcc7c96d04d73d0bcc
Richard Muler <richard.amaral.rj21@gmail.com>
2024-06-04 15:14:22 -0300

    first commit

---
+++ code/calculadora.cpp
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+	//var
+	int op,num1,num2,resultado,resto;
+	float resultadoDiv;
+	
+	//interface
+	printf("-------------------------------------------------------\n");
+	printf("SELECIONE O NUMERO RESPECTIVO PARA A OPERACAO DESEJADA:\n");
+	printf("[1] - ADICAO\n");
+	printf("[2] - SUBTRACAO\n");
+	printf("[3] - MULTIPLICACAO\n");
+	printf("[4] - DIVISAO\n");
+	printf("-------------------------------------------------------\n");
+	
+	//entrada de dados
+	printf("Informe a operacao que deseja realizar:\n");
+	scanf("%d",&op);
+	printf("Informe o primeiro numero:\n");
+	scanf("%d",&num1);
+	printf("Informe o segundo numero:\n");
+	scanf("%d",&num2);
+	
+	//cases
+	switch (op) {
+		case 1:
+			resultado = num1 + num2;
+			printf("Resultado da operacao: %d\n",resultado);
+			break;
+		case 2:
+			resultado = num1 - num2;
+			printf("Resultado da operacao: %d\n",resultado);
+			break;
+		case 3:
+			resultado = num1 * num2;
+			printf("Resultado da operacao: %d\n",resultado);
+			break;
+		case 4:
+			resultadoDiv = num1 / num2;
+			resto = num1 % num2;
+			if (resto != 0) {
+				printf("DIVISAO NAO EXATA!\n");
+			} else {
+				printf("Resultado da operacao: %.1f\n",resultadoDiv);
+			}
+			break;
+		default:
+			printf("DADO INVALIDO\n");
+			break;
+	}
+	system("pause");
+	return 0;
+}
